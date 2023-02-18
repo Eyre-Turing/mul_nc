6174be4d7641b27c814917205928eb8f6aeadb8a
Eyre Turing <771778087@qq.com>
2023-02-18 20:52:58 +0800

    支持unix socket

--- mnc.c
+++ mnc.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -18,9 +19,9 @@
 
 void usage(FILE *fp, const char *self)
 {
-    fprintf(fp, "Usage: %s [-p <port>] [-e <file>] [-c <command>]\n"
-                "  -a <addr>     Address\n"
-                "  -U <path>     Use unix socket. Not implemented, just declared\n"
+    fprintf(fp, "Usage: %s [-Uld] [-a <addr>] [-p <port>] [-e <file>] [-c <command>]\n"
+                "  -a <addr>     Address. If set -U, please spec unix socket file path\n"
+                "  -U            Use unix socket\n"
                 "  -p <port>     Port\n"
                 "  -e <file>     Exec file, whenever a client connected or connected to a server\n"
                 "  -c <command>  Run command by /bin/sh -c, whenever a client connected or connected to a server\n"
@@ -53,10 +54,12 @@ void *read_service(void *arg)
 
 int main(int argc, char *argv[])
 {
-    int serv_fd, cli_fd, opt, port = 0, as_listening = 0, as_service = 0, s;
-    char *tag_host, *port_str = NULL, *exec_file = NULL, *run_command = NULL, *line = NULL, *unix_sock = NULL;
-    struct sockaddr_in my_addr, peer_addr;
-    socklen_t peer_addr_size;
+    int serv_fd, cli_fd, opt, port = 0, as_listening = 0, as_service = 0, use_unix_socket = 0, s;
+    char *tag_host = NULL, *port_str = NULL, *exec_file = NULL, *run_command = NULL, *line = NULL;
+    struct sockaddr *my_addr = NULL, *peer_addr = NULL;
+    struct sockaddr_in my_addr_in, peer_addr_in;
+    struct sockaddr_un my_addr_un, peer_addr_un;
+    socklen_t my_addr_size, peer_addr_size;
     struct addrinfo hints, *result, *rp;
     pid_t pid;
     size_t len = 0;
@@ -65,7 +68,7 @@ int main(int argc, char *argv[])
     struct read_server_arg read_server_arg_t;
 
     // 解析参数
-    while ((opt = getopt(argc, argv, "ha:U:p:e:c:ld")) != -1) {
+    while ((opt = getopt(argc, argv, "ha:Up:e:c:ld")) != -1) {
         switch (opt) {
         case 'h':
             usage(stdout, argv[0]);
@@ -75,7 +78,7 @@ int main(int argc, char *argv[])
             tag_host = optarg;
             break;
         case 'U':
-            unix_sock = optarg;     // 当前只是声明出来，还没实现unix socket，后面实现
+            use_unix_socket = 1;
             break;
         case 'p':
             if (strspn(optarg, "0123456789") != strlen(optarg) || (port = atoi(optarg), port > 65535)) {
@@ -107,35 +110,54 @@ int main(int argc, char *argv[])
     }
 
     // 创建套接字
-    serv_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (use_unix_socket) {
+        serv_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    } else {
+        serv_fd = socket(AF_INET, SOCK_STREAM, 0);
+    }
     if (serv_fd == -1) {
         perror("socket");
         return 1;
     }
 
     if (!as_listening) {    // 如果作为客户端运行
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = 0;
-        hints.ai_protocol = 0;
-        s = getaddrinfo(tag_host, port_str, &hints, &result);
-        if (s != 0) {
-            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        if (!tag_host) {
+            fprintf(stderr, "you have to spec host address.\n");
             close(serv_fd);
             return 1;
         }
-        for (rp = result; rp; rp = rp->ai_next) {
-            if (connect(serv_fd, rp->ai_addr, rp->ai_addrlen) != -1) {
-                break;
+        if (use_unix_socket) {
+            my_addr_un.sun_family = AF_UNIX;
+            strncpy(my_addr_un.sun_path, tag_host, sizeof(my_addr_un.sun_path) - 1);
+            if (connect(serv_fd, (struct sockaddr *) &my_addr_un, sizeof(my_addr_un)) == -1) {
+                perror("connect");
+                close(serv_fd);
+                return 1;
             }
-            perror("connect");
-        }
-        freeaddrinfo(result);
-        if (rp == NULL) {
-            fprintf(stderr, "could not connect\n");
-            close(serv_fd);
-            return 1;
+        } else {
+            memset(&hints, 0, sizeof(hints));
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_STREAM;
+            hints.ai_flags = 0;
+            hints.ai_protocol = 0;
+            s = getaddrinfo(tag_host, port_str, &hints, &result);
+            if (s != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+                close(serv_fd);
+                return 1;
+            }
+            for (rp = result; rp; rp = rp->ai_next) {
+                if (connect(serv_fd, rp->ai_addr, rp->ai_addrlen) != -1) {
+                    break;
+                }
+                perror("connect");
+            }
+            freeaddrinfo(result);
+            if (rp == NULL) {
+                fprintf(stderr, "could not connect\n");
+                close(serv_fd);
+                return 1;
+            }
         }
         if (exec_file || run_command) {
             pid = fork();
@@ -175,10 +197,28 @@ int main(int argc, char *argv[])
     // 以下是作为服务端运行的逻辑
 
     // 绑定端口
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(port);
-    my_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
-    if (bind(serv_fd, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1) {
+    if (use_unix_socket) {
+        if (!tag_host) {
+            fprintf(stderr, "you have to spec unix socket file path.\n");
+            close(serv_fd);
+            return 1;
+        }
+        my_addr_un.sun_family = AF_UNIX;
+        strncpy(my_addr_un.sun_path, tag_host, sizeof(my_addr_un.sun_path) - 1);
+        my_addr = (struct sockaddr *) &my_addr_un;
+        my_addr_size = sizeof(my_addr_un);
+    } else {
+        my_addr_in.sin_family = AF_INET;
+        my_addr_in.sin_port = htons(port);
+        if (tag_host) {
+            my_addr_in.sin_addr.s_addr = inet_addr(tag_host);
+        } else {
+            my_addr_in.sin_addr.s_addr = inet_addr("0.0.0.0");
+        }
+        my_addr = (struct sockaddr *) &my_addr_in;
+        my_addr_size = sizeof(my_addr_in);
+    }
+    if (bind(serv_fd, my_addr, my_addr_size) == -1) {
         perror("bind");
         close(serv_fd);
         return 1;
@@ -196,8 +236,14 @@ int main(int argc, char *argv[])
     // 开始循环
     while (1) {
         // 等待连接
-        peer_addr_size = sizeof(peer_addr);
-        cli_fd = accept(serv_fd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+        if (use_unix_socket) {
+            peer_addr = (struct sockaddr *) &peer_addr_un;
+            peer_addr_size = sizeof(peer_addr_un);
+        } else {
+            peer_addr = (struct sockaddr *) &peer_addr_in;
+            peer_addr_size = sizeof(peer_addr_in);
+        }
+        cli_fd = accept(serv_fd, peer_addr, &peer_addr_size);
         if (cli_fd == -1) {
             perror("accept");
             continue;
